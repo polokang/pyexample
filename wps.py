28285c5f896a7e98780d147af3188b3f0625fdf3
周恒康 <polokang@163.com>
2018-03-21 15:56:52 +0800

    is new

--- wps.py
+++ wps.py
@@ -1,22 +1,42 @@
-# -!- coding: utf-8 -!-
-__author__ = 'Hunter'
-
 from win32com.client import Dispatch
 import win32com.client
 import time
 excel =  win32com.client.Dispatch('Excel.Application')
 excel.Visible = -1
-myBook = excel.Workbooks.Open(r'C:\Users\Hunter\PY\2018.xlsx')
+myBook = excel.Workbooks.Open(r'd:\320.xls')
 mySheet = myBook.Worksheets("1")
-LastRow = mySheet.usedrange.rows.count
-print("have",LastRow,"line")
-mySheet.Activate
-mySheet.Cells(2,2).Value=r"测试用"
+row = mySheet.usedrange.rows.count
+col = mySheet.usedrange.columns.count
+print("have",row,"line")
+print("have",col,"col")
+
+i = 2
+line = ""
+while i <= row:
+    j = 1
+    while j <= col:
+        # print(i-1, '',mySheet.Cells(i,j).Value)
+        line += str(mySheet.Cells(i, j).Value) + " "
+        j += 1
+    print(line)
+    line = ""
+    i += 1
+
+
+# mySheet.Activate
+# mySheet.Cells(2,2).Value=r"测试用"
 
 #get value
-aCellValue = mySheet.Cells(2,3).Value
-print(aCellValue)
+# aCellValue = mySheet.Cells(2,3).Value
+# print(aCellValue)
+print("............done.")
+
+
+reSheet = myBook.Worksheets("2")
+# reSheet.Activate
+reSheet.Cells(2,2).Value = r"测试"
 #save
 myBook.save
+
 #quit
-myBook.close
+# myBook.close
